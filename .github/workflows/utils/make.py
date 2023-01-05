5cc783aca59c65a3b1cf4a79a783647518df7ebd
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-01-05 07:10:32 +0100

    chore: throw when make failed

--- .github/workflows/utils/make.py
+++ .github/workflows/utils/make.py
@@ -26,7 +26,10 @@ def build_all():
     """
     for makefile in get_all_targets():
         print(f"Building {makefile}...")
-        subprocess.run(["make"], cwd=Path(makefile).parent)
+        completed_process = subprocess.run(["make"], cwd=Path(makefile).parent)
+        if completed_process.returncode != 0:
+            raise Exception(f"Failed to build {makefile}")
+        
 
 if __name__ == "__main__":
     fire.Fire()
